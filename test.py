14503ec74a5e7eaeba219d6feda1fbf9e7ed0b3f
Shlok Mathur <shlokmathur@Shloks-MacBook-Air.local>
2023-12-28 19:45:55 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,30 @@
+import openai
+import asyncio
+import re
+import whisper
+import boto3
+import pydub
+from pydub import playback
+import speech_recognition as sr
+from EdgeGPT import Chatbot, ConversationStyle
+
+async def main():
+    bot = Chatbot(cookiePath='cookies.json')
+    response = await bot.ask(prompt=input(""), conversation_style=ConversationStyle.precise)
+
+    for message in response["item"]["messages"]:
+        if message["author"] == "bot":
+            bot_response = message["text"]
+
+    bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
+    # Select only the bot response from the response dictionary
+    for message in response["item"]["messages"]:
+        if message["author"] == "bot":
+            bot_response = message["text"]
+    # Remove [^#^] citations in response
+    bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
+    print(bot_response)
+    await bot.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
